dca9a0fedd01efa5da2371b3b041d41f5411acb7
Neil Rhodes <neil@pobox.com>
2018-09-17 15:39:21 -0700

    Fix cuda

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,8 +6,7 @@ cd ~/downloads/
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604
 _9.0.176-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x8
-6_64/7fa2af80.pub
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt update
 sudo apt install cuda -y
 wget http://files.fast.ai/files/cudnn-9.1-linux-x64-v7.tgz
